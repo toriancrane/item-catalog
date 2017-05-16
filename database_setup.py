1f85c2a74598328f9abea143a5c45a82ee5e2674
Torian <toriancrane@gmail.com>
2017-05-16 08:05:37 -0700

    Pushing project updates

--- database_setup.py
+++ database_setup.py
@@ -17,18 +17,21 @@ class User(Base):
     email = Column(String(250), nullable = False)
     picture = Column(String(250))
 
-class Item(Base):
-    """ Corresponds to the Item table """
+class Game(Base):
+    """ Corresponds to the Game table """
     # Table information
-    __tablename__ = 'item'
+    __tablename__ = 'game'
 
     # Mappers
     id = Column(Integer, primary_key = True)
     name = Column(String(80), nullable = False)
-    description = Column(String(250))
+    description = Column(String(250), nullable = False)
+    genre = Column(String(250), nullable = False)
+    console = Column(String(250), nullable = False)
     price = Column(String(8))
-    
-
+    picture = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 engine = create_engine(
     'sqlite:///restaurantmenuwithusers.db')
