9bba0588f79d6f062d80d3f07fd30dbc4999c81d
toriancrane <toriancrane@gmail.com>
2017-05-18 15:51:39 -0700

    Updating project files

--- project.py
+++ project.py
@@ -9,7 +9,8 @@ from flask import(
     make_response,
     jsonify)
 from functools import wraps
-import random, string
+import random
+import string
 import requests
 import jinja2
 import httplib2
@@ -18,7 +19,7 @@ import time
 import os
 import json
 
-#Additional imports
+# Additional imports
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 
@@ -28,12 +29,14 @@ CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Wild Game Catalog"
 
+
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                for x in xrange(32))
+                    for x in xrange(32))
     login_session['state'] = state
-    return render_template('signin.html', STATE = state)
+    return render_template('signin.html', STATE=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -88,7 +91,8 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
+        response = make_response(json.dumps('Current ' +
+                                            'user is already connected.'),
                                  200)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -131,7 +135,8 @@ def gconnect():
 def gdisconnect():
     credentials = login_session.get('credentials')
     if credentials is None:
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(json.dumps('Current ' +
+                                 'user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = credentials.access_token
@@ -141,20 +146,23 @@ def gdisconnect():
     print 'result is '
     print result
     if result['status'] == '200':
-        del login_session['credentials'] 
+        del login_session['credentials']
         del login_session['gplus_id']
         del login_session['username']
         del login_session['email']
         del login_session['picture']
-        response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
+        response = make_response(redirect('/'))
+        response.headers['Content-Type'] = 'text/html'
         return response
-    else:    
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+    else:
+        response = make_response(json.dumps('Failed to revoke ' +
+                                 'token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
 
-#####   User Login Decorator    #####
+# User Login Decorator
+
+
 def login_required(func):
     """
     A decorator to confirm a user is logged in or redirect as needed.
@@ -168,14 +176,16 @@ def login_required(func):
             return func(*args, **kwargs)
     return check_login
 
+
 def createUser(login_session):
     user_name = login_session['username']
     user_email = login_session['email']
     user_pic = login_session['picture']
     db_methods.addNewUser(user_name, user_email, user_pic)
-    
+
     user_id = db_methods.getUserID(user_email)
     return user_id
+
 
 @app.route('/')
 def frontPage():
@@ -184,7 +194,7 @@ def frontPage():
         return render_template('front.html')
     else:
         user_id = login_session['user_id']
-        return render_template('front.html', user_id = user_id)
+        return render_template('front.html', user_id=user_id)
 
 
 @app.route('/games/')
@@ -193,11 +203,13 @@ def gamesPage():
     games_list = db_methods.getRecentGames()
     genres = db_methods.getAllGenres()
     if 'username' not in login_session:
-        return render_template('publicgames.html', games = games_list, genres = genres)
+        return render_template('publicgames.html', games=games_list,
+                               genres=genres)
     else:
         user_id = login_session['user_id']
-        return render_template('games.html', games = games_list,
-                            genres = genres, user_id = user_id)
+        return render_template('games.html', games=games_list,
+                               genres=genres, user_id=user_id)
+
 
 @app.route('/games/genre/<int:genre_id>/')
 def gamesByGenrePage(genre_id):
@@ -206,12 +218,13 @@ def gamesByGenrePage(genre_id):
     all_genres = db_methods.getAllGenres()
     genre = db_methods.searchGenreByID(genre_id)
     if 'username' not in login_session:
-        return render_template('publicgamesbygenre.html', games = games_list,
-                            genres = all_genres, genre = genre)
+        return render_template('publicgamesbygenre.html', games=games_list,
+                               genres=all_genres, genre=genre)
     else:
         user_id = login_session['user_id']
-        return render_template('gamesbygenre.html', games = games_list,
-                            genres = all_genres, genre = genre, user_id = user_id)
+        return render_template('gamesbygenre.html', games=games_list,
+                               genres=all_genres, genre=genre,
+                               user_id=user_id)
 
 
 @app.route('/games/new/', methods=['GET', 'POST'])
@@ -228,27 +241,29 @@ def newGamePage():
         game_pic = request.form['game_pic']
         game_user = login_session['user_id']
 
-        #Retrieve genre id from genre name
+        # Retrieve genre id from genre name
         genre_id = db_methods.searchGenreIDByName(game_genre)
 
         db_methods.addNewGame(game_name, game_desc, genre_id,
-                            game_price, game_pic, game_user)
+                              game_price, game_pic, game_user)
         time.sleep(0.1)
         game = db_methods.searchGameByName(game_name)
 
         return redirect('/games/%s/info' % game.id)
     else:
-        return render_template('newgame.html', user_id = game_user)
+        return render_template('newgame.html', user_id=game_user)
+
 
 @app.route('/games/<int:game_id>/info/')
 def viewGamePage(game_id):
     """ View Game Info Function """
     game = db_methods.searchGameByID(game_id)
     if 'username' not in login_session:
-        return render_template('publicinfo.html', game = game)
+        return render_template('publicinfo.html', game=game)
     else:
         user_id = login_session['user_id']
-        return render_template('info.html', game = game, user_id = user_id)
+        return render_template('info.html', game=game, user_id=user_id)
+
 
 @app.route('/games/<int:game_id>/edit/', methods=['GET', 'POST'])
 @login_required
@@ -262,7 +277,7 @@ def editGamePage(game_id):
         game_price = request.form['game_price']
         game_pic = request.form['game_pic']
 
-        #Retrieve genre id from genre name
+        # Retrieve genre id from genre name
         genre_id = db_methods.searchGenreIDByName(game_genre)
 
         db_methods.editGame(game_name, game_desc, genre_id,
@@ -271,8 +286,9 @@ def editGamePage(game_id):
         return redirect('/games/%s/info' % game.id)
     else:
         user_id = login_session['user_id']
-        return render_template('editgame.html', game = game, 
-                                user_id = user_id)
+        return render_template('editgame.html', game=game,
+                               user_id=user_id)
+
 
 @app.route('/games/<int:game_id>/delete/', methods=['GET', 'POST'])
 @login_required
@@ -281,23 +297,27 @@ def deleteGamePage(game_id):
     if request.method == 'POST':
         error = game.name + " has been deleted from the database."
         db_methods.deleteGame(game_id)
-        return render_template('deleteconfirmation.html', error = error)
+        return render_template('deleteconfirmation.html', error=error)
     else:
         error = "Are you sure you want to delete this game?"
         user_id = login_session['user_id']
-        return render_template('deletegame.html', error = error,
-                                game = game, user_id = user_id)
+        return render_template('deletegame.html', error=error,
+                               game=game, user_id=user_id)
 
 # JSON API Endpoints
+
+
 @app.route('/games/JSON/')
 def allGamesJSON():
     games = db_methods.getAllGames()
     return jsonify(Game=[g.serialize for g in games])
 
+
 @app.route('/games/<int:game_id>/info/JSON/')
 def gameInfoJSON(game_id):
     game = db_methods.searchGameByID(game_id)
     return jsonify(Game=[game.serialize])
+
 
 @app.route('/games/genre/<int:genre_id>/JSON/')
 def gamesByGenreJSON(genre_id):
@@ -307,5 +327,5 @@ def gamesByGenreJSON(genre_id):
 if __name__ == '__main__':
     app.secret_key = 'gDI1tL5OC54UiTF3g18a-bWg'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8000)
-    #Change this back to be pushed to heroku
+    app.run(host='0.0.0.0', port=8000)
+    # Change this back to be pushed to heroku
